a0a632b0ecb9b8b743887409a4e4ccd3193e928a
Adrian Chand <adidasiscool12345@gmail.com>
2024-10-05 10:27:25 +1300

    added basic operations

--- main.c
+++ main.c
@@ -1,18 +1,40 @@
 #include<stdio.h>
 
+double calculate(double number1, char operator, double number2){
+	switch(operator){
+		case '+':
+			return number1 + number2;
+		case '-': 
+			return number1 - number2;
+		case '*':
+			return number1 * number2;
+		case '/':
+			while(number2 == 0){
+				printf("Error: %lf", number2);
+				return 0;
+			}
+			number1 / number2;
+		default:
+			printf("Error: Invalid operator!");
+			return 0;	
+	}
+}
+
 int main(){
-	float number1, number2;
+	double number1, number2, answer;
 	char operator;
 
 	printf("Enter the first number: ");
-	scanf("%f", &number1);
+	scanf("%lf", &number1);
 
 	printf("Enter an operator {\'+\', \'-\', \'*\', \'/\'} ");
 	scanf(" %c", &operator);
 
 	printf("Enter the second number: ");
-	scanf("%f", &number2);
+	scanf("%lf", &number2);
 
-	printf("These are your selections: %f, %c, %f \n", number1, operator, number2);
+	answer = calculate(number1, operator, number2);
+
+	printf("equals: %lf \n", answer);
 	return 0;
 }	
